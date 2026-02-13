3e12923454b01b3ce7531ba0b5d6b9c27a61ed19
Sebastian Lema <sebasjhonlemamontoya12@gmail.com>
2026-02-13 14:40:35 -0500

    Implementar cliente de socket para conexión y comunicación con el servidor

--- 01_sockets/cliente.py
+++ 01_sockets/cliente.py
@@ -1,1 +1,10 @@
-# Cliente
+import socket
+
+direccion = ("localhost", 65432)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as cliente:
+    print(f"Conectando a {direccion[0]}:{direccion[1]}")
+    cliente.connect(direccion)
+    cliente.sendall(b"Hola, servidor!")
+    respuesta = cliente.recv(1024).decode()
+    print(f"Respuesta: {respuesta}")
+
